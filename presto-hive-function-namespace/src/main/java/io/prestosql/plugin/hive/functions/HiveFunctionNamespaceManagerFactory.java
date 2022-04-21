f767a66fd3b70c87f1be38302c1869b74ae70c11
wyy566 <531938832@qq.com>
2022-04-21 16:20:25 +0800

    add module presto hive function namespace

---
+++ presto-hive-function-namespace/src/main/java/io/prestosql/plugin/hive/functions/HiveFunctionNamespaceManagerFactory.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.prestosql.plugin.hive.functions;
+
+import com.google.inject.Injector;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.log.Logger;
+import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.classloader.ThreadContextClassLoader;
+import io.prestosql.spi.function.FunctionHandleResolver;
+import io.prestosql.spi.function.FunctionNamespaceManager;
+import io.prestosql.spi.function.FunctionNamespaceManagerContext;
+import io.prestosql.spi.function.FunctionNamespaceManagerFactory;
+
+import java.util.Map;
+
+import static io.prestosql.plugin.hive.functions.FunctionRegistry.addFunction;
+import static io.prestosql.plugin.hive.functions.FunctionRegistry.getCurrentFunctionNames;
+import static io.prestosql.plugin.hive.functions.HiveFunctionNamespacePlugin.EXTERNAL_FUNCTIONS_DIR;
+import static java.util.Objects.requireNonNull;
+
+public class HiveFunctionNamespaceManagerFactory
+        implements FunctionNamespaceManagerFactory
+{
+    private static final Logger log = Logger.get(HiveFunctionNamespaceManagerFactory.class);
+
+    private final ClassLoader classLoader;
+    private final FunctionHandleResolver functionHandleResolver;
+
+    public static final String NAME = "hive-functions";
+
+    public HiveFunctionNamespaceManagerFactory(ClassLoader classLoader)
+    {
+        this.classLoader = requireNonNull(classLoader, "classLoader is null");
+        this.functionHandleResolver = new HiveFunctionHandleResolver();
+    }
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public FunctionHandleResolver getHandleResolver()
+    {
+        return functionHandleResolver;
+    }
+
+    @Override
+    public FunctionNamespaceManager<?> create(String catalogName, Map<String, String> config, FunctionNamespaceManagerContext functionNamespaceManagerContext)
+    {
+        requireNonNull(config, "config is null");
+
+        try (ThreadContextClassLoader ignored = new ThreadContextClassLoader(classLoader)) {
+            for (String key : config.keySet()) {
+                try {
+                    if (!getCurrentFunctionNames().contains(key)) {
+                        addFunction(key, classLoader.loadClass(config.get(key)));
+                    }
+                    else {
+                        log.warn("Function %s alredy exists.", config.get(key));
+                    }
+                }
+                catch (ClassNotFoundException e) {
+                    if (!EXTERNAL_FUNCTIONS_DIR.equals(key)) {
+                        log.warn("Invalid parameter %s or class %s not Found.", key, config.get(key));
+                    }
+                }
+            }
+
+            Bootstrap app = new Bootstrap(
+                    new HiveFunctionModule(catalogName, classLoader, functionNamespaceManagerContext.getTypeManager().get()));
+
+            Injector injector = app
+                    .doNotInitializeLogging()
+                    .setRequiredConfigurationProperties(config)
+                    .quiet()
+                    .initialize();
+            return injector.getInstance(FunctionNamespaceManager.class);
+        }
+        catch (PrestoException e) {
+            throw e;
+        }
+    }
+}
---
+++ presto-hive-function-namespace/src/main/java/io/prestosql/plugin/hive/functions/HiveFunctionNamespacePlugin.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.plugin.hive.functions;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.log.Logger;
+import io.prestosql.spi.Plugin;
+import io.prestosql.spi.function.FunctionNamespaceManagerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static io.airlift.configuration.ConfigurationLoader.loadPropertiesFrom;
+import static java.lang.String.format;
+
+public class HiveFunctionNamespacePlugin
+        implements Plugin
+{
+    private static final Logger log = Logger.get(HiveFunctionNamespacePlugin.class);
+    private static final String FUNCTION_PROPERTIES_FILE_PATH = format("etc%sfunction-namespace", File.separatorChar);
+    public static final String EXTERNAL_FUNCTIONS_DIR = "external-functions.dir";
+
+    @Override
+    public Iterable<FunctionNamespaceManagerFactory> getFunctionNamespaceManagerFactories()
+    {
+        return ImmutableList.of(new HiveFunctionNamespaceManagerFactory(getClassLoader()));
+    }
+
+    private ClassLoader getClassLoader()
+    {
+        File path = new File(FUNCTION_PROPERTIES_FILE_PATH);
+        List<URL> urls = new ArrayList<>();
+        for (File file : path.listFiles()) {
+            if (file.isFile() && file.length() != 0) {
+                try {
+                    Map<String, String> config = loadPropertiesFrom(format(FUNCTION_PROPERTIES_FILE_PATH + "%s" + file.getName(), File.separatorChar));
+                    if (config.keySet().contains(EXTERNAL_FUNCTIONS_DIR)) {
+                        urls = getURLs(config.get(EXTERNAL_FUNCTIONS_DIR));
+                        break;
+                    }
+                }
+                catch (IOException e) {
+                    log.error("Read configuration %s fail.", file.getName());
+                }
+            }
+        }
+        return new URLClassLoader(urls.toArray(new URL[urls.size()]), HiveFunctionNamespacePlugin.class.getClassLoader());
+    }
+
+    private List<URL> getURLs(String externalFunctionsDir)
+    {
+        File dir = new File(externalFunctionsDir);
+        List<URL> urls = new ArrayList<>();
+        String dirName = dir.getName();
+        if (!dir.exists() || !dir.isDirectory()) {
+            log.debug("%s doesn't exist or is not a directory.", dirName);
+            return urls;
+        }
+        File[] files = dir.listFiles();
+        if (files == null || files.length == 0) {
+            log.debug("%s is empty.", dirName);
+            return urls;
+        }
+        for (File file : files) {
+            try {
+                urls.add(file.toURI().toURL());
+            }
+            catch (MalformedURLException e) {
+                log.error("Failed to add %s to URLs of HiveFunctionsClassLoader.", file);
+            }
+        }
+        return urls;
+    }
+}
